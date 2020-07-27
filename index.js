87de45a3c94159210a5231cbe44482e477eaf74d
aslan-alt <874470891@qq.com>
2020-07-27 15:23:53 +0800

    重新打包

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import Button from './src/button.vue'
 import ButtonGroup from './src/button-group.vue'
 import Icon from './src/icon.vue'
 
+console.log(Button, ButtonGroup, Icon)
+
 export {
     Button,
     ButtonGroup,
