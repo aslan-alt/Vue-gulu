a6f6fadef6d99008b0c805a53c76b2dd090b0b5d
aslan-alt <874470891@qq.com>
2020-07-29 21:44:27 +0800

    基本实现row col的功能/添加gutter/以及用到scss循环语法

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot />
+  <div
+    class="col"
+    :class="[span && `col-${span}`,offset && `offset-${offset}`]"
+    :style="{paddingLeft:(gutter/2)+'px',paddingRight:(gutter/2)+'px'}"
+  >
+    <div class="xxx">
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -12,21 +18,40 @@ export default {
     span: {
       type: [Number, String],
     },
+    offset: {
+      type: [Number, String],
+    },
+  },
+  data() {
+    return {
+      gutter: 0,
+    };
+  },
+  mounted() {
+    console.log(this.gutter);
   },
 };
 </script>
 <style lang="scss" scoped>
 $width: 4.166666;
+.xxx {
+  border: 1px solid green;
+}
 .col {
   height: 100px;
-  background: grey;
+  // background: grey;
   width: 50%;
-  border: 1px solid red;
 
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n/24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft:-(gutter/2)+'px',marginRight:-(gutter/2)+'px'}">
     <slot />
   </div>
 </template>
@@ -8,6 +8,14 @@
 <script>
 export default {
   name: "aslanRow",
+  props: {
+    gutter: [String, Number],
+  },
+  mounted() {
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter; //篡改了col组件的gutter
+    });
+  },
 };
 </script>
 <style lang="scss" scoped>
