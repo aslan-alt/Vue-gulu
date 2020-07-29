8b381dbd3a932882f9ea987ea70b2e21e10b79d8
aslan-alt <874470891@qq.com>
2020-07-29 19:17:58 +0800

    row 和 col 的雏形完成

---
+++ src/row.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <slot />
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "aslanRow",
+};
+</script>
+<style lang="scss" scoped>
+.row {
+  display: flex;
+}
+</style>
+
