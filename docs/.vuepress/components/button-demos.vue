b79b14f5dddc3588022916a1c95b9b1e15bec4df
aslan-alt <874470891@qq.com>
2020-08-10 19:51:47 +0800

    备份

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <g-button>默认按钮</g-button>
+    <g-button icon="setting">默认按钮</g-button>
+    <g-button :loading="true">默认按钮</g-button>
+  </div>
+</template>
+
+
+<script>
+import Button from "../../../src/button";
+
+export default {
+  components: {
+    'g-button': Button
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
+
--- docs/components/button.md
+++ docs/components/button.md
@@ -4,3 +4,4 @@ title:Button
 # Button
 
 使用方法
+<button-demos class="custom"></button-demos>
